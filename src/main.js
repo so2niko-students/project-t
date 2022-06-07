1e5db5cd20afd1e406ca03cb70033633c87255a7
Naira Kazeian <nkazeian@gmail.com>
2022-06-07 22:23:46 +0300

    replaced to querySelector

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import MessagesView from "./components/MessagesView.js";
 import AuthView from "./components/AuthView.js";
 import MenuView from "./components/MenuView.js";
 
-const authViewContainer = document.getElementById("authView");
-const menuViewContainer = document.getElementById("menuView");
 const MAIN = document.querySelector(".main");
 
-AuthView(authViewContainer);
-MenuView(menuViewContainer);
+AuthView(MAIN);
+MenuView(MAIN);
 MessagesView(MAIN);
