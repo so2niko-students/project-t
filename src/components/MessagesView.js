d47bdfb162ba2e3c15aef4ba069339a4d24312ac
Naira Kazeian <nkazeian@gmail.com>
2022-06-10 14:51:08 +0300

    attempt to render all messages

--- src/components/MessagesView.js
+++ src/components/MessagesView.js
@@ -9,6 +9,7 @@ export default function MessagesView(MAIN) {
   const url = `${urlBase}${apiToken}/getUpdates`;
 
   function render(message) {
+    console.log(message);
     const messagesStr = templateMessages(message);
     const a = `${getTableHead()}${messagesStr}${getTableEnd()}`;
     return (MAIN.innerHTML = a);
@@ -17,20 +18,33 @@ export default function MessagesView(MAIN) {
   function getUpdate(url) {
     return fetch(url)
       .then((d) => d.json())
-      .then((data) => data);
+      .then((data) => {
+        saveUsers(data);
+        return data;
+      });
   }
 
   getUpdate(url).then((r) => {
+    // console.log(r.result);
+    const renderAll = r.result.map((obj) => obj.message);
+    render(renderAll);
     r.result.forEach((allMessages) => {
       messagesStore.dispatch(
         updateMsgAction(allMessages.message || allMessages.edited_message)
       );
-    });
-    messagesStore.getState();
-    const a = messagesStore.getState();
 
-    a.forEach((el) => {
-      render(el);
+      messagesStore.getState();
+      const a = messagesStore.getState();
+      // console.log(a);
+
+      a.forEach((el) => {
+        render(el);
+      });
     });
   });
+
+  function saveUsers(userData) {
+    localStorage.setItem("users", JSON.stringify(userData));
+    //TODO complete with info inside
+  }
 }
